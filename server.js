12c7321092021c0515d96a384fea109ed86342c3
Júlia Degrava <juhcdgomes@gmail.com>
2025-08-25 11:30:18 -0300

    Feat: Finaliza atividade de Harry Potter

--- server.js
+++ server.js
@@ -1,18 +1,137 @@
+// Importar pacotes/bibliotecas
 import express from "express";
-import bruxos from "./src/data/bruxos.js";
+import dados from "./src/data/dados.js";
 
+const { bruxos, casas, varinhas, animais, pocoes } = dados;
+
+// Criar aplicação com Express e configurar para aceitar JSON
 const app = express();
-const portServer = 3000;
+app.use(express.json());
 
-app.get("/", (req,res) => {
-    res.send("Vamos de Harry Potter")
-})
+// Definir constante para porta do servidor
+const serverPort = 3000;
+
+// Rota principal GET para "/"
+app.get("/", (req, res) => {
+    res.send("Vamos de Harry Potter");
+});
 
 app.get("/bruxos", (req, res) => {
-    res.json(bruxos);
+  if (bruxos.length > 0) {
+      res.status(200).json(bruxos);
+  } else {
+      res.status(404).json({
+          mensagem: "Bruxos não encontrados :("
+      })
+  }
 });
 
-app.listen(portServer, () => {
-    console.log("🧙‍♂️ API dos Bruxos está no ar na porta 3000 !");
+app.get("/bruxos/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const bruxo = bruxos.find(p => p.id === id);
+
+  if (bruxo) {
+      res.status(200).json(bruxo);
+  } else {
+      res.status(404).json({
+          mensagem: "Esse bruxo não existe"
+      })
+  }
 });
 
+app.get("/casas", (req, res) => {
+  if (casas.length > 0) {
+      res.status(200).json(casas);
+  } else {
+      res.status(404).json({
+          mensagem: "Casas não encontradas :("
+      })
+  }
+});
+
+app.get("/casas/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const casa = casas.find(p => p.id === id);
+
+  if (casa) {
+      res.status(200).json(casa);
+  } else {
+      res.status(404).json({
+          mensagem: "Esse bruxo não existe"
+      })
+  }
+});
+
+app.get("/varinhas", (req, res) => {
+  if (varinhas.length > 0) {
+      res.status(200).json(varinhas);
+  } else {
+      res.status(404).json({
+          mensagem: "Varinhas não encontradas :("
+      })
+  }
+});
+
+app.get("/varinhas/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const varinha = varinhas.find(p => p.id === id);
+
+  if (varinha) {
+      res.status(200).json(varinha);
+  } else {
+      res.status(404).json({
+          mensagem: "Essa varinha não existe"
+      })
+  }
+});
+
+app.get("/animais", (req, res) => {
+  if (animais.length > 0) {
+      res.status(200).json(animais);
+  } else {
+      res.status(404).json({
+          mensagem: "Animais não encontrados :("
+      })
+  }
+});
+
+app.get("/animais/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const animal = animais.find(p => p.id === id);
+
+  if (animal) {
+      res.status(200).json(animal);
+  } else {
+      res.status(404).json({
+          mensagem: "Esse animal não existe"
+      })
+  }
+});
+
+app.get("/pocoes", (req, res) => {
+  if (pocoes.length > 0) {
+      res.status(200).json(pocoes);
+  } else {
+      res.status(404).json({
+          mensagem: "Poções não encontradas :("
+      })
+  }
+});
+
+app.get("/pocoes/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const pocao = pocoes.find(p => p.id === id);
+
+  if (pocao) {
+      res.status(200).json(pocao);
+  } else {
+      res.status(404).json({
+          mensagem: "Essa poção não existe"
+      })
+  }
+});
+
+// Iniciar servidor escutando na porta definida
+app.listen(serverPort, () => {
+    console.log(`🚀 Servidor rodando em http://localhost:${serverPort} !!!`);
+});
---
+++ src/data/dados.js
@@ -0,0 +1,120 @@
+const bruxos = [
+    {
+        id: 1,
+        nome: "Harry Potter",
+        ano: 7,
+        varinha: "Azevinho e pena de fenix, 28cm",
+        mascote: "Coruja",
+        patrono: "Cervo",
+        especialidade: "sobreviver",
+        vivo: false
+    }
+];
+
+const casas = [
+    {
+        id: 1,
+        nome: "Grifinória",
+        fundador: "Grodic Gryffindor",
+        cores: "Vermelho e dourado",
+        animal: "Leao"
+
+    },
+    {
+        id: 2,
+        nome: "Sonserina",
+        fundador: "Salazar Slytherin",
+        cores: "Verde e Prata",
+        animal: "Serpente"
+    },
+    {
+        id: 3,
+        nome: "Corvinal",
+        fundador: "Rowena Ravenclaw",
+        cores: "Azul e Bronze",
+        animal: "Águia"
+    },
+    {
+        id: 4,
+        nome: "Lufa-Lufa",
+        fundador: "Helga Hufflepuff",
+        cores: "Amarelo e Preto",
+        anmal: "Texugo"
+    }
+];
+
+const varinhas = [
+    {
+        id: 1,
+        material: "Azevinho",
+        nucleo: "Pena de Fênix",
+        comprimento: "28cm",
+    },
+    {
+        id: 2,
+        material: "Videira",
+        nucleo: "Fibra de Coração de Dragão",
+        comprimento: "27cm",
+    },
+    {
+        id: 3,
+        material: "Salgueiro",
+        nucleo: "Pelo de Unicórnio",
+        comprimento: "35cm",
+    },
+    {
+        id: 4,
+        material: "Carvalho Inglês",
+        nucleo: "Pelo de Testrálio",
+        comprimento: "32cm",
+    }
+];    
+
+const animais = [
+    {
+        id: 1,
+        nome: "Coruja",
+        tipo: "Correio/Companhia"
+    },
+    {
+        id: 2,
+        nome: "Gato",
+        tipo: "Companhia"
+    },
+    {
+        id: 3,
+        nome: "Sapo",
+        tipo: "Companhia"
+    },
+    {
+        id: 4,
+        nome: "Rato",
+        tipo: "Companhia"
+    }
+];
+
+const pocoes = [
+    {
+        id: 1,
+        nome: "Polissuco",
+        efeito: "Transforma na aparência de outra pessoa"
+
+    },
+    {
+        id: 2,
+        nome: "Felix Felicis",
+        efeito: "Sorte temporária",
+    },
+    {
+        id: 3,
+        nome: "Amortentina",
+        efeito: "Poção do amor"
+    },
+    {
+        id: 4,
+        nome: "Veritaserum",
+        efeito: "Força a dizer a verdade"
+    }
+]
+
+export default { bruxos, casas, varinhas, animais, pocoes };
